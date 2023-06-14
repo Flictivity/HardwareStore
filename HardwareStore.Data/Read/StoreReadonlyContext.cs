f145eabde9e8cb9c93ca65e0f3d61dd4db55670f
Flictivity <bulat_sa1@mail.ru>
2023-06-14 21:57:09 +0300

    added a database contexts

---
+++ HardwareStore.Data/Read/StoreReadonlyContext.cs
@@ -0,0 +1,25 @@
+﻿using Npgsql;
+
+namespace HardwareStore.Data.Read;
+
+public class StoreReadonlyContext : IDisposable, IAsyncDisposable
+{
+    public NpgsqlConnection Connection { get; }
+
+    public StoreReadonlyContext(string connectionString)
+    {
+        Connection = new NpgsqlConnection(connectionString);
+    }
+
+    public void Dispose()
+    {
+        Connection.Dispose();
+        GC.SuppressFinalize(this);
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await Connection.DisposeAsync();
+        GC.SuppressFinalize(this);
+    }
+}
---
+++ HardwareStore.Data/Write/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HardwareStore.Data.Write;
+
+public class StoreContext : DbContext
+{
+    public StoreContext()
+    {
+    }
+
+    public StoreContext(DbContextOptions<StoreContext> options)
+        : base(options)
+    {
+    }
+}
